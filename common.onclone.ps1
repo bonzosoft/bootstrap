3c975d467ffbc6fd7b7cb5577aa79dd7ad76c0b9
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-20 16:40:24 +0200

    .

--- common.onclone.ps1
+++ common.onclone.ps1
@@ -37,8 +37,11 @@ else {
 }
 
 ## Set file permisisons for volumes
+Write-Host "pasa1"
 [hashtable]$compose = Get-DockerCompose -Path $Script:workingComposeFile
+Write-Host "pasa2"
 [string[]]$volumes = Get-DockerVolumes $compose
+Write-Host "pasa3"
 Write-Host "Volumes:"
 $volumes
 

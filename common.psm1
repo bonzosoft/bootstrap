5dea1602675a0a06b5325e00d83a6f9bb893800e
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-20 20:40:20 +0200

    .

--- common.psm1
+++ common.psm1
@@ -450,7 +450,7 @@ function Get-DockerCompose {
         throw "File $($Path.FullName) not found."
     }
 
-    $content = docker compose -f $workingFile.FullName config
+    $content = docker compose -f $Path.FullName config
         #--no-consistency		Don't check model consistency - warning: may produce invalid Compose output
         #--no-env-resolution	Don't resolve service env files
         #--no-interpolate		Don't interpolate environment variables
@@ -484,7 +484,7 @@ function Get-DockerVolumes {
             }
         }
     }
-    return $volumes
+    return $volumesList
 }
 
 

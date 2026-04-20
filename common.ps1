b90497cbec7035f1ade9186f7126eb98e877ff81
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-20 13:53:35 +0200

    .

--- common.ps1
+++ common.ps1
@@ -510,7 +510,7 @@ function Get-DockerVolumes {
     [string[]]$volumes = @()
 
     foreach ($service in $Compose.services) {
-        Write-Host "Inspeccionando el servicio $($service.name)"
+        Write-Host "Inspeccionando el servicio $service"
         if ($service.volumes) {
             foreach ($volume in $service.volumes) {
                 if ($volume.type -eq "bind") {

df3831e80cb48269cbe4074f5a0aceb7ab8359bf
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-20 16:26:47 +0200

    .

--- common.ps1
+++ common.ps1
@@ -515,7 +515,7 @@ function Get-DockerVolumes {
         foreach ($serviceName in $service.Keys ) {
             if ($service.$serviceName.Keys -Contains "volumes") {
                 Write-host "Volumen encontrado"
-                $service.$serviceName.volumes.Values
+                #$service.$serviceName.volumes.Values
                 $service.$serviceName.volumes.source
                 $volumes += $service.$serviceName.volumes.source
             }

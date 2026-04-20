07276c19a65c266570f849c85e76956766c98813
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-20 12:26:21 +0200

    .

--- common.onclone.ps1
+++ common.onclone.ps1
@@ -26,7 +26,7 @@ if (Test-Path -Path $Script:IncludeDir) {
     }
 
     ## RUN INCLUDE SCRIPTS
-    [IO.FileSystemInfo]$scripts = Get-Item -Path $Script:IncludeDir -Filter "*/onclone.ps1"
+    [IO.FileSystemInfo]$scripts = Get-Item -Path (Join-Path -Path $Script:IncludeDir -ChildPath "*/onclone.ps1")
     $scripts
     foreach ($script in $scripts) {
         Write-Host "Loading submodule script '$($script.FullName)'."

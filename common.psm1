5956ed52056618b27cc07dd8f81b047d49d7e749
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-20 20:47:08 +0200

    .

--- common.psm1
+++ common.psm1
@@ -480,7 +480,7 @@ function Get-DockerVolumes {
             if ($service.$serviceName.Keys -Contains "volumes") {
                 #Write-host "Volumen encontrado"
                 #$service.$serviceName.volumes.source
-                $volumesList.AddRange($service.$serviceName.volumes.source)
+                $volumesList.AddRange([string[]]$service.$serviceName.volumes.source)
             }
         }
     }

42eea3e511a22c285f096fd7c983e199f843efe1
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-23 20:03:09 +0200

    .

--- posh-Docker/1.0.0/public/functions/Get-DockerCompose.ps1
+++ posh-Docker/1.0.0/public/functions/Get-DockerCompose.ps1
@@ -8,20 +8,24 @@ function Get-DockerCompose {
         [IO.FileInfo]$Path
     )
 
-    [string[]]$content = @()
+    [string[]]$fileContent = @()
+    [hashtable]$dockerCompose = @{}
 
     if (-not (Test-Path -Path $Path.FullName)) {
         throw "File $($Path.FullName) not found."
     }
 
-    $content = docker compose -f $Path.FullName config
+    $fileContent = docker compose -f $Path.FullName config 2>&1
         #--no-consistency		Don't check model consistency - warning: may produce invalid Compose output
         #--no-env-resolution	Don't resolve service env files
         #--no-interpolate		Don't interpolate environment variables
         #--no-normalize		    Don't normalize compose model (convierte formatos cortos a largos)
         #--no-path-resolution	Don't resolve file paths
     if ($LASTEXITCODE) {
-        throw "Unable to generate compose file: $($Error[0])"
-    }   
-    return [hashtable]($content | ConvertFrom-Yaml)
+        throw "Unable to generate compose file: $fileContent"
+    }
+
+    $dockerCompose = $fileContent | ConvertFrom-Yaml
+
+    return $dockerCompose
 }

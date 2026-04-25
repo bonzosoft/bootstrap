966bb215fe8600e0ff82b0db2db26e82676fa657
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-25 10:06:06 +0200

    .

--- common.onpull.ps1
+++ common.onpull.ps1
@@ -47,10 +47,10 @@ if (Test-Path -Path $Script:INCLUDEDIR) {
 
 
 ## Set file permisisons for volumes
-[Collections.Generic.List[IO.FileSystemInfo]]$volumes= Get-DockerVolumes -Data (Get-DockerCompose -Path $Script:COMPOSEFILE)
-foreach ($volume in $volumes) {
-    Grant-DockerPermission -Path $volume -PUID $Script:PUID -PGID $Script:PGID -Mode "0755" -Recurse -Force
-}
+#[Collections.Generic.List[IO.FileSystemInfo]]$volumes= Get-DockerVolumes -Data (Get-DockerCompose -Path $Script:COMPOSEFILE)
+#foreach ($volume in $volumes) {
+#    Grant-DockerPermission -Path $volume -PUID $Script:PUID -PGID $Script:PGID -Mode "0755" -Recurse -Force
+#}
 
 ## Get Docker PGID
 #if (Test-Path -Path $Script:CONFIGJSON) {

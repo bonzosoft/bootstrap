b6de9b9c419ad6272e8581092e3c402aca7f6e64
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-26 14:00:40 +0200

    .

--- common.onpull.ps1
+++ common.onpull.ps1
@@ -37,8 +37,8 @@ if (Test-Path -Path $Script:IncludeDir) {
 
     ## LOAD SUBMODULE SCRIPTS
     Write-Host "Loading submodule scripts."
-    [IO.FileInfo[]]$submoduleScripts = @(Get-Item -Path (Join-Path -Path $Script:IncludeDir -ChildPath "*" -AdditionalChildPath (Split-Path -Path $PSCommandPath -Leaf)))
-    Join-Path -Path $Script:IncludeDir -ChildPath "*" -AdditionalChildPath (Split-Path -Path $PSCommandPath -Leaf)
+    [IO.FileInfo[]]$submoduleScripts = @(Get-Item -Path (Join-Path -Path $Script:IncludeDir -ChildPath "*" -AdditionalChildPath (Split-Path -Path $MyInvocation.PSCommandPath -Leaf)))
+    Join-Path -Path $Script:IncludeDir -ChildPath "*" -AdditionalChildPath (Split-Path -Path $MyInvocation.PSCommandPath -Leaf)
     $submoduleScripts
     foreach ($script in $submoduleScripts) {
         Write-Host "Running submodule script '$($script.FullName)'."

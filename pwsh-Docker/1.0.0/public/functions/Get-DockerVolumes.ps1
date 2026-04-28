74e9149f3e4d003bed6f6298acc40be31bd50d55
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-28 12:25:43 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Get-DockerVolumes.ps1
+++ pwsh-Docker/1.0.0/public/functions/Get-DockerVolumes.ps1
@@ -8,12 +8,17 @@ function Get-DockerVolumes {
         [hashtable]$Data
     )
 
+    write-host "pasa"
     [IO.FileSystemInfo[]]$volumesList = @()
 
     foreach ($service in $Data.services.Keys) {
+        write-host "pasa"
+
         $Data.services.$service
         if ($Data.services.$service.Keys -like "volumes") {
             foreach ($volume in $Data.services.$service.volumes.source) {
+                write-host "pasa"
+
                 if (Test-Path -Path $volume) {
                     $volumesList += Get-Item -Path $volume
                 }

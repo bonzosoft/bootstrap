d29cc7ac5feb650bef70c23eb4e936d5675bc599
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-28 20:47:24 +0200

    .

--- pwsh-Docker/1.0.0/pwsh-Docker.psm1
+++ pwsh-Docker/1.0.0/pwsh-Docker.psm1
@@ -30,7 +30,7 @@ $Script:Env | Add-Member -MemberType NoteProperty -Name HostInfo         -Value 
 $Script:Env | Add-Member -MemberType NoteProperty -Name PUID             -Value [int]568
 $Script:Env | Add-Member -MemberType NoteProperty -Name PGID             -Value [int]568
 $Script:Env | Add-Member -MemberType NoteProperty -Name DOCKER_PGID      -Value [int]$Env.HostInfo.DOCKER_PGID
-$Script:Env | Add-Member -MemberType NoteProperty -Name IS_TRUENAS       -Value [int]$Env.HostInfo.IS_TRUENAS
+$Script:Env | Add-Member -MemberType NoteProperty -Name IS_TRUENAS       -Value [bool]$Env.HostInfo.IS_TRUENAS
 
 
 ### Look for module assets #####################################################

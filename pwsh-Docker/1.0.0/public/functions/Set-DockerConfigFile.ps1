03f48d754b14a585e3131f0d50b38f20a4ebdda4
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-04-29 16:15:25 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Set-DockerConfigFile.ps1
+++ pwsh-Docker/1.0.0/public/functions/Set-DockerConfigFile.ps1
@@ -19,6 +19,7 @@ function Set-DockerConfigFile {
     )
    
     process {
+        Write-Host $MyInvocation | Format-List *
         foreach ($item in $Path) {
             if ($Link.IsPresent) {
                 New-Item -Path $Target -ItemType SymbolicLink -Value $item.FullName -Force:$Force | Out-Null

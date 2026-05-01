130e99152662207b2b0e3e9026c2574b85e65751
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-01 10:38:37 +0200

    .

--- run.ps1
+++ run.ps1
@@ -56,6 +56,7 @@ function Write-Log {
 
 function Get-DockerPGID {
     $group = Get-Content "/host/etc/group" | Where-Object { $_ -match "^docker:" }
+    Write-Host "pasa por aqui"
     if ($group.Count -eq 1) {
         return [int]($group.Split(":")[2])
     }

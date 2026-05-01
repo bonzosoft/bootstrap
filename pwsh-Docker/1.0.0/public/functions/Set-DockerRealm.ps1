33aac94ef2283f87ffe9d281b8b4085c393490eb
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-01 09:38:14 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Set-DockerRealm.ps1
+++ pwsh-Docker/1.0.0/public/functions/Set-DockerRealm.ps1
@@ -6,6 +6,9 @@ function Set-DockerRealm {
     
     [IO.FileInfo]$source = Join-Path -Path $Script:Context.DotEnvFile.Directory -ChildPath "$($Script:Context.DotEnvFile.Name)$($Script:Context.HostInfo.REALM)"
     [IO.FileInfo]$target = $Context.DotEnvFile
+
+    Write-Host "source: $source"
+    Write-Host "target: $target"
     
     if (Test-Path -Path $source) {
         New-Item -Path $target -ItemType SymbolicLink -Value $source -Force | Out-Null

6d15745bb390da1259e20d6824ff7fef393f31e3
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-01 10:46:33 +0200

    .

--- run.ps1
+++ run.ps1
@@ -55,7 +55,7 @@ function Write-Log {
 }
 
 function Get-DockerPGID {
-    $group = Get-Content "/host/etc/group" | Where-Object { $_ -match "^docker:" }
+    [string[]]$group = Get-Content "/host/etc/group" | Where-Object { $_ -match "^docker:" }
     Write-Host "pasa por aqui"
     $group
     if ($group.Count -eq 1) {

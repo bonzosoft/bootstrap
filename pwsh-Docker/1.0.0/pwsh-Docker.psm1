ed961e459681268e4acd9154c8bd04a749bad20f
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-07 19:35:24 +0200

    .

--- pwsh-Docker/1.0.0/pwsh-Docker.psm1
+++ pwsh-Docker/1.0.0/pwsh-Docker.psm1
@@ -23,28 +23,28 @@ Export-ModuleMember -Variable *
 
 ### Private variables ##########################################################
 [PSCustomObject]$Script:Context = [PSCustomObject]@{}
-$Script:Context | Add-Member -MemberType NoteProperty -Name WorkingDir  -Value ([IO.DirectoryInfo](Get-Location).Path)
-$Script:Context | Add-Member -MemberType NoteProperty -Name ConfigDir   -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath "config"))
-$Script:Context | Add-Member -MemberType NoteProperty -Name IncludeDir  -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath "include"))
-$Script:Context | Add-Member -MemberType NoteProperty -Name ComposeFile -Value ([IO.FileInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath "compose.yaml"))
-$Script:Context | Add-Member -MemberType NoteProperty -Name DotEnvFile  -Value ([IO.FileInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath ".env"))
+$Script:Context | Add-Member -MemberType NoteProperty -Name WorkingDir     -Value ([IO.DirectoryInfo](Get-Location).Path)
+$Script:Context | Add-Member -MemberType NoteProperty -Name ConfigDir      -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath "config"))
+$Script:Context | Add-Member -MemberType NoteProperty -Name IncludeDir     -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath "include"))
+$Script:Context | Add-Member -MemberType NoteProperty -Name ComposeFile    -Value ([IO.FileInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath "compose.yaml"))
+$Script:Context | Add-Member -MemberType NoteProperty -Name DotEnvFile     -Value ([IO.FileInfo](Join-Path -Path $Script:Context.WorkingDir -ChildPath ".env"))
 
-$Script:Context | Add-Member -MemberType NoteProperty -Name ProjectName -Value ([string]$Script:Context.WorkingDir.BaseName)
-$Script:Context | Add-Member -MemberType NoteProperty -Name DataDir     -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.WorkingDir.Parent.Parent -ChildPath "state" -AdditionalChildPath $Script:Context.ProjectName))
-$Script:Context | Add-Member -MemberType NoteProperty -Name SecretsDir  -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.DataDir -ChildPath ".secrets"))
+$Script:Context | Add-Member -MemberType NoteProperty -Name ProjectName    -Value ([string]$Script:Context.WorkingDir.BaseName)
+$Script:Context | Add-Member -MemberType NoteProperty -Name DataDir        -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.WorkingDir.Parent.Parent -ChildPath "state" -AdditionalChildPath $Script:Context.ProjectName))
+$Script:Context | Add-Member -MemberType NoteProperty -Name SecretsDir     -Value ([IO.DirectoryInfo](Join-Path -Path $Script:Context.DataDir -ChildPath ".secrets"))
 
-$Script:Context | Add-Member -MemberType NoteProperty -Name HostInfo    -Value (Get-Content -Path (Join-Path -Path $Script:Context.WorkingDir.Parent -ChildPath ".config" -AdditionalChildPath "docker.config.json") | ConvertFrom-Json)
+$Script:Context | Add-Member -MemberType NoteProperty -Name HostInfo       -Value (Get-Content -Path (Join-Path -Path $Script:Context.WorkingDir.Parent -ChildPath ".config" -AdditionalChildPath "docker.config.json") | ConvertFrom-Json)
 
-$Script:Context | Add-Member -MemberType NoteProperty -Name APP_PUID    -Value 568
-$Script:Context | Add-Member -MemberType NoteProperty -Name APP_PGID    -Value $Script:Context.APP_PUID
-$Script:Context | Add-Member -MemberType NoteProperty -Name PROXY_PUID  -Value $Script:Context.APP_PUID
-$Script:Context | Add-Member -MemberType NoteProperty -Name PROXY_PGID  -Value $Script:Context.APP_PGID
-$Script:Context | Add-Member -MemberType NoteProperty -Name DB_PUID     -Value $Script:Context.APP_PUID
-$Script:Context | Add-Member -MemberType NoteProperty -Name DB_PGID     -Value $Script:Context.APP_PGID
-$Script:Context | Add-Member -MemberType NoteProperty -Name SOCKET_PUID -Value $Script:Context.APP_PUID
-$Script:Context | Add-Member -MemberType NoteProperty -Name SOCKET_PGID -Value $Script:Context.APP_PGID
-$Script:Context | Add-Member -MemberType NoteProperty -Name WORKER_PUID -Value $Script:Context.APP_PUID
-$Script:Context | Add-Member -MemberType NoteProperty -Name WORKER_PGID -Value $Script:Context.APP_PGID
+$Script:Context | Add-Member -MemberType NoteProperty -Name APP_PUID       -Value 568
+$Script:Context | Add-Member -MemberType NoteProperty -Name APP_PGID       -Value 568
+$Script:Context | Add-Member -MemberType NoteProperty -Name WEBPROXY_PUID  -Value $Script:Context.APP_PUID
+$Script:Context | Add-Member -MemberType NoteProperty -Name WEBPROXY_PGID  -Value $Script:Context.APP_PGID
+$Script:Context | Add-Member -MemberType NoteProperty -Name DB_PUID        -Value $Script:Context.APP_PUID
+$Script:Context | Add-Member -MemberType NoteProperty -Name DB_PGID        -Value $Script:Context.APP_PGID
+$Script:Context | Add-Member -MemberType NoteProperty -Name SOCKPROXY_PUID -Value $Script:Context.APP_PUID
+$Script:Context | Add-Member -MemberType NoteProperty -Name SOCKPROXY_PGID -Value $Script:Context.APP_PGID
+$Script:Context | Add-Member -MemberType NoteProperty -Name WORKER_PUID    -Value $Script:Context.APP_PUID
+$Script:Context | Add-Member -MemberType NoteProperty -Name WORKER_PGID    -Value $Script:Context.APP_PGID
 
 
 ### Look for module assets #####################################################

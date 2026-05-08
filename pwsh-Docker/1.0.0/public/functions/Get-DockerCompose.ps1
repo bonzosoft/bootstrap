5d5e03639e756b446665e8a28c0c55ceb77221b6
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-08 22:40:21 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Get-DockerCompose.ps1
+++ pwsh-Docker/1.0.0/public/functions/Get-DockerCompose.ps1
@@ -18,21 +18,19 @@ function Get-DockerCompose {
             throw "File $($Path.FullName) not found."
         }
     
-        Write-Host "pasa por aqui 1"
-        $composeLines = docker compose -f $Path.FullName config 2>&1
+        $composeLines = docker compose -f $Path.FullName config 2> variable:errorMessage
             #--no-consistency		Don't check model consistency - warning: may produce invalid Compose output
             #--no-env-resolution	Don't resolve service env files
             #--no-interpolate		Don't interpolate environment variables
             #--no-normalize		    Don't normalize compose model (convierte formatos cortos a largos)
             #--no-path-resolution	Don't resolve file paths
         if ($LASTEXITCODE) {
-            throw "Unable to parse '$Path':`n$composeLines"
+            throw "Unable to parse '$Path': $errorMessage"
         }
-        Write-Host "pasa por aqui 2"
-        Write-Host $composeLines
-        Write-Host "pasa por aqui 3"
-        $composeData = $composeLines | ConvertFrom-Yaml
     
+        Write-Host $errorMessage
+
+        $composeData = $composeLines | ConvertFrom-Yaml
         Write-Output $composeData
     }
 }

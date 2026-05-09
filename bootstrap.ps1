f6dcf057b57447bb025d2d395d29c0a665a82464
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-09 12:29:57 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -46,7 +46,7 @@ $ErrorActionPreference = "Stop"
 [int]$PGID = 568
 [IO.DirectoryInfo]$Script:COMMONDIR = Join-Path -Path (Get-Location) -ChildPath "common"
 [IO.FileInfo]$Script:CONFIGFILE = Join-Path -Path (Get-Location) -ChildPath ".config" -AdditionalChildPath "docker.config.json"
-export GH_CONFIG_DIR=${PWD}/.config/gh
+$env:GH_CONFIG_DIR=(Join-Path -Path ${PWD} -ChildPath ".config/gh")
 # =========================
 # Helpers
 # =========================

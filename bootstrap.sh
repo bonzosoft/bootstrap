fdb4e044f001855948b428f9b8b84ae6897860a5
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-09 13:26:27 +0200

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ docker run \
     -v /etc:/host/etc:ro \
     -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
     -w ${PWD} \
-    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ./bootstrap/bootstrap.ps1
+    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ./bootstrap/bootstrap.ps1 -Verbose

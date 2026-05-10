9b40eab8b8999c62c43232b697ffcbe1494c60ad
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-10 23:56:52 +0200

    .

--- common.onpull.ps1
+++ common.onpull.ps1
@@ -42,7 +42,7 @@ if (Test-Path -Path $Script:Context.IncludeDir) {
 Set-DockerVariable -Name DATADIR          -Value $Script:Context.DataDir.FullName
 Set-DockerVariable -Name INCLUDEDIR       -Value $Script:Context.IncludeDir.FullName
 Set-DockerVariable -Name SECRETSDIR       -Value $Script:Context.SecretsDir.FullName
-Set-DockerVariable -Name SOCKETPROXY_PGID -Value (Get-DockerGid)
+Set-DockerVariable -Name SOCKETPROXY_PGID -Value (Get-DockerGid) -NoAppend
 $Script:Context
 
 

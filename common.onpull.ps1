804175c13f372b69a114c780ef66d94f5e701fb2
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-10 22:43:06 +0200

    .

--- common.onpull.ps1
+++ common.onpull.ps1
@@ -34,7 +34,7 @@ $Script:Context
 $composeData = Get-DockerCompose -Path $script:Context.ComposeFile
 $composeData.services.Keys | Get-DockerVolumes -InputObject $composeData
 $volumes = Get-DockerVolumes -InputObject $composeData -Service $composeData.services.Keys
-Write-Host $volumes | Out-String
+$volumes
 
 
 ## Set file permisisons for volumes

7db344f1777e991da4c9bf3ee40b321d42ec2a9e
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-10 23:47:56 +0200

    .

--- common.onpull.ps1
+++ common.onpull.ps1
@@ -52,7 +52,7 @@ $volumes = Get-DockerVolumes -InputObject $composeData -Service $composeData.ser
 Set-DockerContext -Name "Service" -Value $volumes
 $Script:Context 
 $Script:Context.Service.db.Volume.FullName
-
+$Script:Context.Service.Keys
 foreach ($service in $volumes.Keys) {
     Write-Information -Message "Configuring storage for service $($service)"
     $volumes.$service.Path.FullName

000c0222a35c9eca202fc5691ec3552c73c3bfa1
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-11 21:00:24 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Set-DockerSecret.ps1
+++ pwsh-Docker/1.0.0/public/functions/Set-DockerSecret.ps1
@@ -43,7 +43,7 @@ function Set-DockerSecret {
         [IO.FileInfo]$secretFile = Join-Path -Path $Path.FullName -ChildPath $Name
         [IO.FileInfo]$temporaryFile = $null
         [string]$currentValue = ""
-        [IO.UnixFileMode]$filePermission = [IO.UnixFileMode]::UserRead + [IO.UnixFileMode]::GroupRead
+        [IO.UnixFileMode]$filePermission = [IO.UnixFileMode]::UserRead + [IO.UnixFileMode]::UserWrite
 
     }
 

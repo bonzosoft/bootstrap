c0aeeb99dfaa2c0cc45657ea6cdf2d3f1a665d99
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-12 19:33:38 +0200

    .

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,4 @@ docker run \
     -v /etc:/host/etc:ro \
     -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
     -w ${PWD} \
-    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -Command "$@"
+    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -Command "$@" -InformationAction Continue

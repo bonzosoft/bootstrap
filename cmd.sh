71537602af304341c2ea2f41ebe86bbb9711d65a
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-17 18:29:31 +0200

    .

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,19 @@
-docker run \
-    --rm \
-    -v /etc:/host/etc:ro \
-    -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
-    -w ${PWD} \
-    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -Command "$@" -InformationAction Continue
+if [ $# -gt 0 ]; then
+    docker run \
+        --rm \
+        -it \
+        -v /etc:/host/etc:ro \
+        -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
+        -v /var/run/docker.sock:/var/run/docker.sock:rw \
+        -w ${PWD} \
+        ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -Command "$@" -InformationAction Continue
+else
+    docker run \
+        --rm \
+        -it \
+        -v /etc:/host/etc:ro \
+        -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
+        -v /var/run/docker.sock:/var/run/docker.sock:rw \
+        -w ${PWD} \
+        ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile
+fi
--- console.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-    --rm \
-    -it \
-    -v /etc:/host/etc:ro \
-    -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
-    -v /var/run/docker.sock:/var/run/docker.sock:rw \
-    -w ${PWD} \
-    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -Command pwsh

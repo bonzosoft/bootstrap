04f7fc4816271b5301fb30d153c1ab39763c02b3
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-17 02:38:40 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Get-DockerCompose.ps1
+++ pwsh-Docker/1.0.0/public/functions/Get-DockerCompose.ps1
@@ -18,7 +18,7 @@ function Get-DockerCompose {
             throw "File '$($Path.FullName)' not found."
         }
     
-        $lines = docker compose -f $Path.FullName config --no-env-resolution 2> variable:errorMessage
+        $lines = docker compose -f $Path.FullName config --no-interpolate 2> variable:errorMessage
             #--no-consistency		Don't check model consistency - warning: may produce invalid Compose output
             #--no-env-resolution	Don't resolve service env files
             #--no-interpolate		Don't interpolate environment variables

c4e03406e8968e2ed58e654011bd578da59d6796
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-18 18:31:03 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Switch-DockerTenant.ps1
+++ pwsh-Docker/1.0.0/public/functions/Switch-DockerTenant.ps1
@@ -14,7 +14,8 @@ function Switch-DockerTenant {
 
     end {
         if (Test-Path -Path $source) {
-            New-Item -Path $target -Value $source -ItemType SymbolicLink -Force | Out-Null
+            #New-Item -Path $target -Value $source -ItemType SymbolicLink -Force | Out-Null
+            Copy-Item -Path $source -Destination $target -Force | Out-Null
         }
         else {
             throw "Source '.env' file not found."

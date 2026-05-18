afea2e02052dbfaec14fd47e7c80118249961546
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-18 20:50:05 +0200

    .

--- common.context.ps1
+++ common.context.ps1
@@ -5,8 +5,10 @@ $Script:Context = @{
     "tenant" =      (Get-Content -Path (Join-Path -Path $WorkingDir.Parent -ChildPath ".config" -AdditionalChildPath "docker.config.json") | ConvertFrom-Json).TENANT
     "path"= @{
         "workingDir" =  $WorkingDir
+        "secretsDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir -ChildPath "include")
         "dataDir" =     [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName))
         "secretsDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName, ".secrets"))
+        
         "composeFile" = [IO.FileInfo](Join-Path -Path $WorkingDir -ChildPath "compose.yaml")
         "dotEnvFile" =  [IO.FileInfo](Join-Path -Path $WorkingDir -ChildPath ".env")
         "configFile" =  [IO.FileInfo](Join-Path -Path $WorkingDir.Parent -ChildPath "" -AdditionalChildPath @(".config", "docker.config.json"))

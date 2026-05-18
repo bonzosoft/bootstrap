c26fb77d1bbdfc3bc07a21256ef1b4447d6be69c
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-18 21:27:45 +0200

    .

--- common.context.ps1
+++ common.context.ps1
@@ -1,18 +1,15 @@
 $WorkingDir = ([IO.DirectoryInfo](Get-Location).Path)
 
 $Script:Context = @{
+    "workingDir" =  $WorkingDir
+    "includeDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir -ChildPath "include")
+    "dataDir" =     [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName))
+    "secretsDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName, ".secrets"))
+    "composeFile" = [IO.FileInfo](Join-Path -Path $WorkingDir -ChildPath "compose.yaml")
+    "dotEnvFile" =  [IO.FileInfo](Join-Path -Path $WorkingDir -ChildPath ".env")
+    "configFile" =  [IO.FileInfo](Join-Path -Path $WorkingDir.Parent -ChildPath "" -AdditionalChildPath @(".config", "docker.config.json"))
     "hostname" =    [string](Get-DockerHostname)
     "tenant" =      (Get-Content -Path (Join-Path -Path $WorkingDir.Parent -ChildPath ".config" -AdditionalChildPath "docker.config.json") | ConvertFrom-Json).TENANT
-    "path"= @{
-        "workingDir" =  $WorkingDir
-        "includeDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir -ChildPath "include")
-        "dataDir" =     [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName))
-        "secretsDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName, ".secrets"))
-        
-        "composeFile" = [IO.FileInfo](Join-Path -Path $WorkingDir -ChildPath "compose.yaml")
-        "dotEnvFile" =  [IO.FileInfo](Join-Path -Path $WorkingDir -ChildPath ".env")
-        "configFile" =  [IO.FileInfo](Join-Path -Path $WorkingDir.Parent -ChildPath "" -AdditionalChildPath @(".config", "docker.config.json"))
-    }
     "docker" = @{
         "projectname"=  [string]($WorkingDir.BaseName)
         "PUID" =        [int]568

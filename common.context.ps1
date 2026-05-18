e2c661114c2076b94a307ba554da4d169a6365a3
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-05-18 20:50:23 +0200

    .

--- common.context.ps1
+++ common.context.ps1
@@ -5,7 +5,7 @@ $Script:Context = @{
     "tenant" =      (Get-Content -Path (Join-Path -Path $WorkingDir.Parent -ChildPath ".config" -AdditionalChildPath "docker.config.json") | ConvertFrom-Json).TENANT
     "path"= @{
         "workingDir" =  $WorkingDir
-        "secretsDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir -ChildPath "include")
+        "includeDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir -ChildPath "include")
         "dataDir" =     [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName))
         "secretsDir" =  [IO.DirectoryInfo](Join-Path -Path $WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $WorkingDir.BaseName, ".secrets"))
         

7802fb845736791ea282f12a2d1560cc154abfd8
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-06-04 21:36:29 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/New-DockerPassword.ps1
+++ pwsh-Docker/1.0.0/public/functions/New-DockerPassword.ps1
@@ -60,16 +60,14 @@ function New-DockerPassword {
                 if (-not (Get-Command -Name "argon2" -ErrorAction SilentlyContinue)) {
                     throw "Command 'argon2' not found. Please install 'argon2' package."
                 }
-                
-                
+                [byte[]]$saltBytes = [System.Security.Cryptography.RandomNumberGenerator]::GetBytes(16)
+                $hexSalt = ($saltBytes | ForEach-Object { '\x' + $PSItem.ToString('x2') }) -join ''
+                $hashedString = /bin/bash -c "echo -n '$plainString' | argon2 `$(printf $hexSalt) -id -t 3 -k 65536 -p 1 -e"
 
-                #[byte[]]$saltBytes = [System.Security.Cryptography.RandomNumberGenerator]::GetBytes(16)
-                #$hexSalt = ($saltBytes | ForEach-Object { '\x' + $PSItem.ToString('x2') }) -join ''
-                #$hashedString = /bin/bash -c "echo -n '$plainString' | argon2 `$(printf $hexSalt) -id -t 3 -k 65536 -p 1 -e"
-                $salt = [System.Convert]::ToBase64String([System.Security.Cryptography.RandomNumberGenerator]::GetBytes(32))
-                $salt = openssl rand -base64 32
-                Write-Warning $salt
-                $plainString | argon2 $salt -id -t 3 -k 65536 -p 1 -e
+                #$salt = [System.Convert]::ToBase64String([System.Security.Cryptography.RandomNumberGenerator]::GetBytes(32))
+                #$salt = openssl rand -base64 32
+                #$plainString | argon2 $salt -id -t 3 -k 65536 -p 1 -e
+
                 if ($LASTEXITCODE) {
                     throw "A problem was found hashing the password."
                 }

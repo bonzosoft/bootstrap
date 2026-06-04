30c54442a506872a4de1b3efe200a04cb7a561b5
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-06-04 19:03:09 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/New-DockerPassword.ps1
+++ pwsh-Docker/1.0.0/public/functions/New-DockerPassword.ps1
@@ -7,22 +7,22 @@ function New-DockerPassword {
         [securestring]$Password = $null,
 
         [Parameter(Mandatory, ParameterSetName = "Plain")]
-        [switch]$Plain,
+        [switch]$AsPlainText,
 
         [Parameter(Mandatory, ParameterSetName = "Base64")]
-        [switch]$Base64,
+        [switch]$AsBase64,
 
         [Parameter(Mandatory, ParameterSetName = "Base64Url")]
-        [switch]$Base64Url,
+        [switch]$AsBase64Url,
 
         [Parameter(Mandatory, ParameterSetName = "Jwt")]
-        [switch]$Jwt,
+        [switch]$AsJwtSecret,
 
         [Parameter(Mandatory, ParameterSetName = "Argon2")]
-        [switch]$Argon2,
+        [switch]$AsArgon2,
 
         [Parameter(Mandatory, ParameterSetName = "BCrypt")]
-        [switch]$BCrypt,
+        [switch]$AsBCrypt,
 
         [Parameter()]
         [int]$Length = 32
@@ -45,88 +45,30 @@ function New-DockerPassword {
             "Plain" {
                 $hashedString = $seed
             }
-            "Base64" {
+            ("Base64" -or "Jwt") {
                 $hashedString = [Convert]::ToBase64String($seed)
             }
             "Base64Url" {
                 #$hashedString = [System.Buffers.Text.Base64Url]::EncodeToString([Text.Encoding]::UTF8.GetBytes($seed))
-                $hashedString = [Convert]::ToBase64String($bytes).TrimEnd('=').Replace('+','-').Replace('/','_')
-            }
-            "Jwt" {
-                $hashedString = [Convert]::ToBase64String($seed)
+                $hashedString = [Convert]::ToBase64String($seed).TrimEnd('=').Replace('+','-').Replace('/','_')
             }
             "Argon2" {
                 if (-not (Get-Command -Name "argon2" -ErrorAction SilentlyContinue)) {
                     throw "Command 'argon2' not found. Please install 'argon2' package."
                 }
-                #$salt = [Convert]::ToBase64String([System.Security.Cryptography.RandomNumberGenerator]::GetBytes(16))
-                #$hexString = [System.Convert]::ToHexString([System.Security.Cryptography.RandomNumberGenerator]::GetBytes(16))
-                #Write-Information "salt: $hexStringt"
-                #$caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
                 $chars = "0123456789ABCDEF"
+                # generating salt
                 $salt = -join ((1..16) | ForEach-Object { 
                     $chars[[System.Security.Cryptography.RandomNumberGenerator]::GetInt32(0, $chars.Length)] 
                 })
-
-                #$salt = "aaaaaaaaaaaaaaaa"
-                #$password = "tu_contraseña_aqui" # Reemplaza por la misma contraseña que usaste en it-tools
-                
-                # 2. Convertimos "aaaaaaaaaaaaaaaa" a un formato de escape: "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-                # Bash interpreta esto a nivel de bytes sin corromper nada.
-                Write-Warning $salt
+                # converting salt to hex
                 $salt = $salt -replace '(..)', '\x$1'
-                Write-Warning $salt
                 
-                
-                # 3. Construimos el comando completo.
-                # Usamos $(printf ...) para que bash genere los bytes en bruto en ese mismo instante.
-                # (El parámetro -e le dice a argon2 que devuelva el string final $argon2id$...)
-                $plainpass = $(ConvertFrom-SecureString -SecureString $Password -AsPlainText)
-                #$hashedString = /bin/bash -c "echo -n '$seed' | argon2 `$(printf '$salt') -id -t 3 -k 65536 -p 1 -e"
-                $hashedString = /bin/bash -c "echo -n '$plainpass' | argon2 `$(printf '$salt') -id -t 3 -k 65536 -p 1 -e"
-                Write-Warning $hashedString
-                Write-Warning $plainpass
+                $hashedString = /bin/bash -c "echo -n '$(ConvertFrom-SecureString -SecureString $Password -AsPlainText)' | argon2 `$(printf '$seed') -id -t 3 -k 65536 -p 1 -e"
 
-                #$hashedString = [System.Text.Encoding]::UTF8.GetString($seed) | argon2 $salt -id -t 3 -k 65536 -p 1 -l $Length -e
-
-
-                ### powershell
-                ## argon 2
-                #Add-Type -Path "./Konscious.Security.Cryptography.Argon2.dll"
-                #
-                #$password = "changeme"
-                #
-                ## salt aleatorio
-                #$salt = New-Object byte[] 16
-                #[System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($salt)
-                #
-                ## convertir password
-                #$passwordBytes = [System.Text.Encoding]::UTF8.GetBytes($password)
-                #
-                ## crear Argon2id
-                #$argon2 = New-Object Konscious.Security.Cryptography.Argon2id($passwordBytes)
-                #
-                #$argon2.Salt = $salt
-                #$argon2.Iterations = 4
-                #$argon2.MemorySize = 65536   # 64 MB
-                #$argon2.DegreeOfParallelism = 2
-                #
-                ## hash
-                #$hash = $argon2.GetBytes(32)
-                #
-                ## base64 para guardar
-                #$hashB64 = [Convert]::ToBase64String($hash)
-                #$saltB64 = [Convert]::ToBase64String($salt)
-                #
-                #$hashB64
-                #$saltB64
-
-
-
-
-                #if ($LASTEXITCODE) {
-                #    throw "A problem was found hashing the password."
-                #}
+                if ($LASTEXITCODE) {
+                    throw "A problem was found hashing the password."
+                }
             }
             "BCrypt" {
                 if (-not (Get-Command -Name "mkpasswd" -ErrorAction SilentlyContinue)) {

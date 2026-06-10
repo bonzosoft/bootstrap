20b6d2d3dbc9b84320feae7a718fc7692ed2b6b1
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-06-11 00:06:16 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Set-DockerContext.ps1
+++ pwsh-Docker/1.0.0/public/functions/Set-DockerContext.ps1
@@ -27,6 +27,7 @@ function Set-DockerContext {
         $context.StateDir        = [IO.DirectoryInfo](Join-Path -Path $context.WorkingDir.Parent.Parent -ChildPath "" -AdditionalChildPath @("state", $context.ProjectName))
         $context.SecretsDir      = [IO.DirectoryInfo](Join-Path -Path $context.StateDir                 -ChildPath "" -AdditionalChildPath @(".secrets"))
         # CommonDir
+        Write-Information $MyInvocation.MyCommand.Path
         $context.CommonDir       = [IO.DirectoryInfo]($MyInvocation.MyCommand.Path)  # ([IO.FileInfo]$PSCommandPath).Directory
         
         # Docker

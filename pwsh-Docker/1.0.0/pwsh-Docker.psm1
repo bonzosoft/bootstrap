74e433d8fa4463965692ec2e254f319fa46fc017
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-06-10 23:01:24 +0200

    .

--- pwsh-Docker/1.0.0/pwsh-Docker.psm1
+++ pwsh-Docker/1.0.0/pwsh-Docker.psm1
@@ -17,7 +17,7 @@ Write-Verbose -Message "Loading module '$($Self.BaseName)'."
 
 
 ### Public variables ###########################################################
-[hashtable]$Script:Context = Get-DockerContext
+[hashtable]$Script:Context = Set-DockerContext
 
 # export public variables
 Export-ModuleMember -Variable *

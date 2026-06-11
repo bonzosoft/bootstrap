d597c1e31b596ff2cca267e95abafea6a85c618e
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-06-11 22:43:12 +0200

    .

--- pwsh-Docker/1.0.0/public/functions/Set-DockerContext.ps1
+++ pwsh-Docker/1.0.0/public/functions/Set-DockerContext.ps1
@@ -40,7 +40,7 @@ function Set-DockerContext {
         $context.TenantsFile     =      [IO.FileInfo](Join-Path -Path $context.TenantsDir               -ChildPath "" -AdditionalChildPath @("$($config.Tenant).json")) #@("$((Get-Content -Path $context.HostConfigFile | ConvertFrom-Json).Tenant).json"))
         $context.Tenant          =           [string]($context.TenantsFile.BaseName)
         $tenantInfo = Get-Content -Path $context.TenantsFile | ConvertFrom-Json -Depth 9 -AsHashTable
-        $context += $tenantInfor
+        $context += $tenantInfo
         
               
         $context

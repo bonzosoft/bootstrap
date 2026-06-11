fbd75b6b78dffdfbed99fce329b757ba4f0e767d
bonzoalex <201332122+bonzoalex@users.noreply.github.com>
2026-06-11 20:25:28 +0200

    .

--- common.ps1
+++ common.ps1
@@ -25,7 +25,7 @@ $VerbosePreference = $verboseBackup
 
 
 ### SETUP CONTEXT ##############################################################
-$Script:Context 
+#$Script:Context 
 
 $context = Set-DockerContext
 
@@ -34,8 +34,8 @@ $context
 exit 1
 
 
-$tenantData = Join-Path -Path $Script:Context.CommonDir -ChildPath "tenants" -AdditionalChildPath "$($Script:Context.Tenant).json"
-$Script:Context += Get-Content -Path $tenantData -Encoding utf8 | ConvertFrom-Json -AsHashTable -Depth 9
+#$tenantData = Join-Path -Path $Script:Context.CommonDir -ChildPath "tenants" -AdditionalChildPath "$($Script:Context.Tenant).json"
+#$Script:Context += Get-Content -Path $tenantData -Encoding utf8 | ConvertFrom-Json -AsHashTable -Depth 9
 
 Write-Verbose -Message ($Script:Context | Out-String)
 

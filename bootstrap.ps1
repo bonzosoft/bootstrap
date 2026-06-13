057825845ac69dca808f700de977f47e05f426fb
alex <bonzoalex@bonzoalex.com>
2026-06-13 13:49:24 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -9,19 +9,19 @@ $InformationPreference = 'Continue'
 [IO.DirectoryInfo]$ConfigDir  = Join-Path -Path $WorkingDir -ChildPath @(".config")
 
 Write-Host ""
-Write-Host "Bienvenido al asistente de instalación (v0.1.5). Pulsa una tecla para continuar..."
+Write-Host "Bienvenido al asistente de instalación (v0.1.6). Pulsa una tecla para continuar..."
 Read-Host
 
 $env:GH_CONFIG_DIR=(Join-Path -Path $ConfigDir -ChildPath @("gh"))
 gh config set prompt disabled
 if (-not (gh auth status)) {
     gh auth login --git-protocol "https" --hostname "github.com" --web
-    #gh auth setup-git
+    
 }
-
 if (Test-Path $RepoDir) {
     Remove-Item -Path $RepoDir -Recurse -Force
 }
+gh auth setup-git
 git clone --branch $BranchName --single-branch https://github.com/bonzosoft/$($RepoDir.Name).git
 foreach ($item in @("install", "cmd")) {
     ln -snf (Join-Path -Path $RepoDir -ChildPath @("$item.sh") ) (Join-Path -Path $WorkingDir -ChildPath @("$item"))

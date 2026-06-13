88890a90fe2d2470f25bc0fea8e64cc2eec558b5
alex <bonzoalex@bonzoalex.com>
2026-06-13 12:10:45 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,11 +2,19 @@
 
 $InformationPreference = 'Continue'
 
-[System.IO.DirectoryInfo]$WorkingDir = (Get-Location).Path
-[System.IO.DirectoryInfo]$GHConfigDir = Join-Path -Path $WorkingDir -ChildPath "" -AdditionalChildPath @(".config", "gh")
-[System.IO.FileInfo]$HostConfigFile = Join-Path -Path $WorkingDir -ChildPath "" -AdditionalChildPath @(".config", "host", "config.json")
+[IO.DirectoryInfo]$WorkingDir = (Get-Location).Path
+[IO.DirectoryInfo]$RepoDir    = Join-Path -Path $WorkingDir -ChildPath @("common")
+[IO.DirectoryInfo]$ConfigDir  = Join-Path -Path $WorkingDir -ChildPath @(".config")
+[IO.FileInfo]$HostConfigFile  = Join-Path -Path $ConfigDir  -ChildPath @("host", "config.json")
+[string]$BranchName = "pruebas"
 
+Write-Host "Bienvenido al asistente de instalación. Pulsa una tecla para continuar..."
+Read-Host
 
-Write-Host "workingdir: ${WorkingDir}"
-Write-Host "information: $GHConfigDir"
-Write-Information "information: $HostConfigFile"
+Remove-Item -Path $RepoDir -Recurse -Force
+gh auth login
+git clone --branch $BranchName --single-branch https://github.com/bonzosoft/$($RepoDir.Name).git
+foreach ($item in @("install", "cmd")) {
+    ln -snf (Join-Path -Path $RepoDir -ChildPath @("$item.ps1") ) (Join-Path -Path $WorkingDir -ChildPath @("$item"))
+    chmod +x (Join-Path -Path $WorkingDir -ChildPath @("$item"))
+}

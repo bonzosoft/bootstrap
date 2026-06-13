0c1ba55805e043202e43103c6a312d8acdbeb6d4
alex <bonzoalex@bonzoalex.com>
2026-06-13 12:33:11 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -7,16 +7,18 @@ $InformationPreference = 'Continue'
 [IO.DirectoryInfo]$ConfigDir  = Join-Path -Path $WorkingDir -ChildPath @(".config")
 [string]$BranchName = "pruebas"
 
-Write-Host "v0.1.0"
+Write-Host "v0.1.1"
 Write-Host "Bienvenido al asistente de instalación. Pulsa una tecla para continuar..."
 Read-Host
 
 $env:GH_CONFIG_DIR=(Join-Path -Path $ConfigDir -ChildPath @("gh"))
-gh auth login --git-protocol "https" --web --clipboard 
+gh auth login --git-protocol "https" --hostname "github.com" --web 
 
 if (Test-Path $RepoDir) {
     Remove-Item -Path $RepoDir -Recurse -Force
 }
+gh auth refresh
+gh auth setup-git
 git clone --branch $BranchName --single-branch https://github.com/bonzosoft/$($RepoDir.Name).git
 foreach ($item in @("install", "cmd")) {
     ln -snf (Join-Path -Path $RepoDir -ChildPath @("$item.ps1") ) (Join-Path -Path $WorkingDir -ChildPath @("$item"))

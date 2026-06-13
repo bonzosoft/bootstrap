23c5bf64aba65bbdf5cd1fc9fc3e6a78ddd624c2
alex <bonzoalex@bonzoalex.com>
2026-06-13 10:57:12 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,7 @@
 #!/usr/bin/env pwsh
 
+$InformationPreference = 'Continue'
+
 [System.IO.DirectoryInfo]$WorkingDir = (Get-Location).Path
 [System.IO.DirectoryInfo]$GHConfigDir = Join-Path -Path $WorkingDir -ChildPath "" -AdditionalChildPath @(".config", "gh")
 [System.IO.FileInfo]$HostConfigFile = Join-Path -Path $WorkingDir -ChildPath "" -AdditionalChildPath @(".config", "host", "config.json")
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,2 @@
 
-wget https://raw.githubusercontent.com/bonzosoft/bootstrap/pruebas/bootstrap.ps1 && \
-docker run --rm -it -v /etc:/host/etc:ro -v ${PWD}:${PWD}:rw -w ${PWD} ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ./bootstrap.ps1 -InformationAction Continue && \
-rm ./bootstrap.ps1
+wget -q https://raw.githubusercontent.com/bonzosoft/bootstrap/pruebas/bootstrap.ps1 && docker run --rm -it -v /etc:/host/etc:ro -v ${PWD}:${PWD}:rw -w ${PWD} ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ${PWD}/bootstrap.ps1  && rm ${PWD}/bootstrap.ps1

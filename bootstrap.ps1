02119daec17713adb18a75411f327042feadd27d
alex <bonzoalex@bonzoalex.com>
2026-06-13 12:11:46 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -11,8 +11,11 @@ $InformationPreference = 'Continue'
 Write-Host "Bienvenido al asistente de instalación. Pulsa una tecla para continuar..."
 Read-Host
 
-Remove-Item -Path $RepoDir -Recurse -Force
 gh auth login
+
+if (Test-Path $RepoDir) {
+    Remove-Item -Path $RepoDir -Recurse -Force
+}
 git clone --branch $BranchName --single-branch https://github.com/bonzosoft/$($RepoDir.Name).git
 foreach ($item in @("install", "cmd")) {
     ln -snf (Join-Path -Path $RepoDir -ChildPath @("$item.ps1") ) (Join-Path -Path $WorkingDir -ChildPath @("$item"))

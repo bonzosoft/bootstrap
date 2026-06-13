5d3093e9c6792b2471b0e1b757c9d9cb56116061
alex <bonzoalex@bonzoalex.com>
2026-06-13 10:47:17 +0200

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,4 @@
-docker run \
-    --rm \
-    -it \
-    -v /etc:/host/etc:ro \
-    -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
-    -w ${PWD} \
-    ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ./bootstrap/bootstrap.ps1 -InformationAction Continue
+
+wget https://raw.githubusercontent.com/bonzosoft/bootstrap/pruebas/bootstrap.ps1 && \
+docker run --rm -it -v /etc:/host/etc:ro -v ${PWD}:${PWD}:rw -w ${PWD} ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ./bootstrap.ps1 -InformationAction Continue && \
+rm ./bootstrap.ps1

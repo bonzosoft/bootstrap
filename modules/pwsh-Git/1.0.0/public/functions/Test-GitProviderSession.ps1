84d1979cfeb061aef4c5f1289c424560627fe332
alex <bonzoalex@bonzoalex.com>
2026-06-22 20:02:38 +0200

    .

--- modules/pwsh-Git/1.0.0/public/functions/Test-GitProviderSession.ps1
+++ modules/pwsh-Git/1.0.0/public/functions/Test-GitProviderSession.ps1
@@ -5,22 +5,36 @@ function Test-GitProviderSession {
     param (
         [Parameter()]
         [ValidateSet("github.com")]
-        [string]$Provider = "github.com"
+        [string]$Provider = "github.com",
+
+        [Parameter()]
+        [switch]$GH,
+
+        [Parameter()]
+        [switch]$Git
     )
 
     begin {
-
+        if ((-not $GH.IsPresent) -and (-not $Git.IsPresent)) {
+            $GH = $true
+            $Git = $true
+        }
+        
     }
 
     process {
-        gh auth status --hostname $Provider *> $null
-        if ($LASTEXITCODE) {
-            return $false
+        if ($GH) {
+            gh auth status --hostname $Provider *> $null
+            if ($LASTEXITCODE) {
+                return $false
+            }
         }
-
-        printf "protocol=https\nhost=github.com\n\n" | git credential fill *> $null
-        if ($LASTEXITCODE) {
-            return $false
+        
+        if ($Git) {
+            printf "protocol=https\nhost=github.com\n\n" | git credential fill *> $null
+            if ($LASTEXITCODE) {
+                return $false
+            }
         }
         
         return $true

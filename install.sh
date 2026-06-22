65719fe2e61e856302e577c9aa8784720a6f0f3f
alex <bonzoalex@bonzoalex.com>
2026-06-22 19:02:09 +0200

    .

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Script Mode
-#-v /var/run/docker.sock:/var/run/docker.sock:ro \
 docker run \
     --rm \
     -it \
     -v /etc:/host/etc:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v /mnt/tank0/apps:/mnt/tank0/apps:rw \
     -w ${PWD} \
     ghcr.io/bonzosoft/pwsh pwsh -NoLogo -NoProfile -File ${PWD}/common/install.ps1 -InformationAction Continue

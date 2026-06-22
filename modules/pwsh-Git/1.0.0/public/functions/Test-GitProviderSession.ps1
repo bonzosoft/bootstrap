40e288b1579a2f036e8f724530c21976ee1d8fa8
alex <bonzoalex@bonzoalex.com>
2026-06-22 19:44:19 +0200

    .

--- modules/pwsh-Git/1.0.0/public/functions/Test-GitProviderSession.ps1
+++ modules/pwsh-Git/1.0.0/public/functions/Test-GitProviderSession.ps1
@@ -15,14 +15,17 @@ function Test-GitProviderSession {
     process {
         gh auth status --hostname $Provider *> $null
         if ($LASTEXITCODE) {
+            Write-Host "No GH session."
             return $false
         }
         else {
             (printf "protocol=https\nhost=github.com\n\n" | git credential fill) *> $null
             if ($LASTEXTICODE) {
+                Write-Host "No Git session."
                 return $false
             }
             else {
+                Write-Host "Correct GH and Git session."
                 return $true
             }
         }

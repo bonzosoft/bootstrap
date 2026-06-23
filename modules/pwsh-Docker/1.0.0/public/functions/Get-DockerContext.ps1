a8e0946ef1fc135c99b9307cd152d3a41495cd36
alex <bonzoalex@bonzoalex.com>
2026-06-23 14:33:02 +0200

    .

--- modules/pwsh-Docker/1.0.0/public/functions/Get-DockerContext.ps1
+++ modules/pwsh-Docker/1.0.0/public/functions/Get-DockerContext.ps1
@@ -41,8 +41,7 @@ function Get-DockerContext {
         if (-not $context.Tenant) {
             throw "Tenant name is mandatory."
         }
-        $context.TenantsDir      = [IO.DirectoryInfo](Join-Path -Path $context.CommonDir  -ChildPath @("tenants"))
-        $context.TenantsFile     =      [IO.FileInfo](Join-Path -Path $context.TenantsDir -ChildPath @("$($context.Tenant.ToLower()).json"))
+        $context.TenantsFile     =      [IO.FileInfo](Join-Path -Path $context.CommonDir -ChildPath @("tenants", "$($context.Tenant.ToLower()).json"))
         if (-not (Test-Path $context.TenantsFile)) {
             throw "Unknown tenant name."
         }

49d8aeea000217d0295a917705ccdadf6d3e922f
alex <bonzoalex@bonzoalex.com>
2026-06-23 13:56:33 +0200

    .

--- install.ps1
+++ install.ps1
@@ -185,8 +185,8 @@ trap {
                 
                 foreach ($item in @("install", "cmd")) {
                     Write-Information -MessageData "Adding link '${item}'." -InformationAction 'Continue'
-                    ln -snf (Join-Path -Path $RepoDir -ChildPath @($item + ".sh") ) (Join-Path -Path $WorkingDir -ChildPath @($item))
-                    chmod +x (Join-Path -Path $WorkingDir -ChildPath @($item))
+                    ln -snf (Join-Path -Path ${PWD} -ChildPath @($commonGitRepository, $item + ".sh") ) (Join-Path -Path ${PWD} -ChildPath @($item))
+                    chmod +x (Join-Path -Path ${PWD} -ChildPath @($item))
                 }
             }
             "5" {

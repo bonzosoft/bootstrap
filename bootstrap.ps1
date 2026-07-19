b7a4b0dded5dabc4be0b3c26926fade7ab46a46d
alex <bonzoalex@bonzoalex.com>
2026-07-19 21:54:59 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -94,7 +94,7 @@ begin {
     $configData | ConvertTo-Json -Depth 9 | Set-Content -Path $configFile
 
     Write-host "mostrando token"
-    Write-host $configData
+    Write-host ($configData | Out-string)
     $configData.Git.Token = bw get password "TOKEN_GITHUB_READONLY_ALL" --session $configData.Vault.Session 2> variable:errStream
     if ($LASTEXITCODE -ne 0) {
         throw ($errStream -join [Environment]::NewLine)

1a68688aa2e99e28fdc8f793ffdca99ade3eb1ff
alex <bonzoalex@bonzoalex.com>
2026-08-30 19:37:57 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -198,9 +198,11 @@ try {
 
     "Fetching repository token from vault." | Write-Log
     #$commonRepositorySplat.Token = Get-VaultSecret -Vault $vault -Name "GITHUB_CONTENTS_READONLY_COMMON" -Path "/" | Select-Object -First 1 #| ConvertTo-SecureString -AsPlainText -ErrorAction 'Stop'
-    $commonRepositorySplat.Token = Get-VaultSecret -Vault $vault -Name "GITHUB_CONTENTS_READONLY_COMMON" -Path "/" | 
-        Select-Object -First 1 | 
-        ConvertTo-SecureString -AsPlainText #-ErrorAction 'Stop'
+    Get-VaultSecret -Vault $vault -Name "GITHUB_CONTENTS_READONLY_COMMON" -Path "/"
+
+    $commonRepositorySplat.Token = Get-VaultSecret -Vault $vault -Name "GITHUB_CONTENTS_READONLY_COMMON" -Path "/" #| 
+        #Select-Object -First 1 | 
+        #ConvertTo-SecureString -AsPlainText #-ErrorAction 'Stop'
     Write-Log -Success
     $commonRepositorySplat 
     $commonRepositorySplat.Token = $commonRepositorySplat.Token | ConvertTo-SecureString -AsPlainText

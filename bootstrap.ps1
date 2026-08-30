ae04b38814e405df1b669f2148a97ef3904ac530
alex <bonzoalex@bonzoalex.com>
2026-08-30 19:01:18 +0200

    .

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -197,9 +197,11 @@ try {
 
 
     "Fetching repository token from vault." | Write-Log
-    $commonRepositorySplat.Token = Get-VaultSecret -Vault $vault -Name "GITHUB_CONTENTS_READONLY_COMMON" -Path "/" | ConvertTo-SecureString -AsPlainText -ErrorAction 'Stop'
+    $commonRepositorySplat.Token = Get-VaultSecret -Vault $vault -Name "GITHUB_CONTENTS_READONLY_COMMON" -Path "/" #| ConvertTo-SecureString -AsPlainText -ErrorAction 'Stop'
     Write-Log -Success
+    $commonRepositorySplat
 
+    exit
     
     "Creating repository object." | Write-Log
     [pscustomobject]$repository = $null
